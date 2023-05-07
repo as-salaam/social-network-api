14bdd378cc5274959d62bb77f43f556ed3448efb
mehrdod11 <mehrdod.rahmatov@bk.ru>
2023-05-07 12:02:33 +0500

    Creating auth-register

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -1,1 +1,61 @@
 package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/softclub-go-0-0/instagram-api-service/internal/models"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+	"net/http"
+)
+
+type UserRegistrationData struct {
+	Login        string `json:"login" binding:"required"`
+	Password     string `json:"password" binding:"required"`
+	Confirmation string `json:"confirmation" binding:"required"`
+}
+
+func (h *Handler) Register(c *gin.Context) {
+	var userData UserRegistrationData
+	if err := c.ShouldBindJSON(&userData); err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	var user models.User
+	if result := h.DB.Where("login = ?", userData.Login).First(&user); result.Error == nil {
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"message": "There is a user registered with such email already",
+		})
+		return
+	}
+
+	if userData.Password != userData.Confirmation {
+		log.Println("passwords doesn't match")
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"message": "Passwords doesn't match",
+		})
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userData.Password), 12)
+	if err != nil {
+		log.Print("generating password hash:", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"message": "Internal Server Error",
+		})
+		return
+	}
+
+	user.Login = userData.Login
+	user.Password = string(hashedPassword)
+
+	if result := h.DB.Create(&user); result.Error != nil {
+		log.Println("inserting user data to DB:", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"message": "Internal Server Error",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
+}
--- internal/models/auth.go
+++ internal/models/auth.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type Credentials struct {
+	Login    string `json:"login" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type Claims struct {
+	UserID uuid.UUID
+	jwt.RegisteredClaims
+}
